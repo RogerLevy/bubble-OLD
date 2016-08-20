682dd645afaf6a17b1162f2cdc2a30122179b834
rogerlevy <roger.levy@gmail.com>
2016-08-20 13:45:44 -0400

    add "cheatsheet" to idioms.f

--- lib/idioms.f
+++ lib/idioms.f
@@ -29,8 +29,56 @@
 \     one.
 \  `breadth` the variable that stores the maximum # of accesory idioms the next
 \     idiom can have.  it is reset to 10 every time an idiom is created.
-\  `public` - set current wordlist for defining to current idiom's "publics"
-\  `private` - set current wordlist for defining to current idiom's "privates"
+\  `public` - set current wordlist for defining new words to the current idiom's "publics"
+\  `private` - set current wordlist for defining new words to the current idiom's "privates"
+
+
+\ Cheatsheet:
+\  Create:
+\    `INCLUDE` a file that declares an idiom that isn't already defined in the current search order.
+\  Enter:
+\    Call the name of an idiom.  Its public and private words, predecessor's public words,
+\    and imported public words, will be available.  This is the search order from first to last.
+\      1) Idiom's publics
+\      2) Idiom's privates
+\      3) Imported idioms' publics
+\      4) Predecessor idiom's publics
+\  Public/Private:
+\    Call `PUBLIC` and `PRIVATE` to switch between defining public and private words of the current
+\    idiom.  Private words won't be available to any other idiom, unless you explicitly export them.
+\  Exit:
+\    There is no explicit word to "exit" an idiom.
+\    Instead you enter a different idiom, or call `GLOBAL`, which turns idioms "off" and resets
+\    the search order and current wordlist to `FORTH-WORDLIST`.
+\  Debug:
+\    Call `.IDIOM`.  The imported idioms and parents of the current idiom will be shown.
+\  Inherit:
+\    Declare or enter an idiom.
+\    `INCLUDE` file that declares child idiom
+\    This new idiom will "know" the public words of all of its predecessors.
+\  Extend:
+\    `INCLUDE` a file that declares an idiom that already exists in the current search order.
+\     or
+\    `INCLUDE` a file that at the top simply enters the idiom you want to extend.
+\  Import:
+\    `IMPORT` a file that declares an idiom.  If it's already defined in the current search order, it
+\    will simply be imported, and the rest of the file will be skipped.
+\  Isolate:
+\    In a file, enter any "predecessor" idiom (or say `GLOBAL`) and then declare/extend an idiom.
+\    If you `IMPORT` this file, it will be imported into the current idiom in the importing file.
+\    If you `INCLUDE` this file, it will NOT be imported.
+\    In both cases, the name of the isolated idiom will only be available in its parent.
+\    Therefore, it can only be imported by "related" idioms.
+\  Encapsulate:
+\    The idioms imported by an idiom, and its parents, are NOT imported along with it
+\    when impored into another idiom.  Only its public words.
+\  Export:
+\    The private or public wordlists of the current idiom can be given names that can
+\    be automatically defined within the parent idiom or the Forth wordlist.
+\    Use `@publics | @privates EXPORT-WORDLIST <name>  \ naming convention:  <idiomname>ing`
+\    If for whatever reason both wordlists are desired, export the @privates and import or
+\    extend the idiom.
+
 
 variable 'idiom
 variable breadth  8 breadth !
