e9637f3cfd1b52895a2bf046909c942a3e00b772
rogerlevy <roger.levy@gmail.com>
2016-08-22 19:29:01 -0400

    add DECLARED to idiom.f and cheatsheet

--- lib/idiom.f
+++ lib/idiom.f
@@ -32,13 +32,58 @@
 \  `public` - set current wordlist for defining to current idiom's "publics"
 \  `private` - set current wordlist for defining to current idiom's "privates"
 
+\ Cheatsheet:
+\  Create:
+\    `INCLUDE` a file that declares an idiom that isn't already defined in the current search order.
+\  Enter:
+\    Call the name of an idiom.  Its public and private words, predecessor's public words,
+\    and imported public words, will be available.  This is the search order from first to last.
+\      1) Idiom's publics
+\      2) Idiom's privates
+\      3) Imported idioms' publics
+\      4) Predecessor idiom's publics
+\  Public/Private:
+\    Call `PUBLIC` and `PRIVATE` to switch between defining public and private words of the current
+\    idiom.  Private words won't be available to any other idiom, unless you explicitly export them.
+\  Exit:
+\    There is no explicit word to "exit" an idiom.
+\    Instead you enter a different idiom, or call `GLOBAL`, which turns idioms "off" and resets
+\    the search order and current wordlist to `FORTH-WORDLIST`.
+\  Debug:
+\    Call `.IDIOM`.  The imported idioms and parents of the current idiom will be shown.
+\  Inherit:
+\    Declare or enter an idiom.
+\    `INCLUDE` file that declares child idiom
+\    This new idiom will "know" the public words of all of its predecessors.
+\  Extend:
+\    `INCLUDE` a file that declares an idiom that already exists in the current search order.
+\     or
+\    `INCLUDE` a file that at the top simply enters the idiom you want to extend.
+\  Import:
+\    `IMPORT` a file that declares an idiom.  If it's already defined in the current search order, it
+\    will simply be imported, and the rest of the file will be skipped.
+\  Isolate:
+\    In a file, enter any "predecessor" idiom (or say `GLOBAL`) and then declare/extend an idiom.
+\    If you `IMPORT` this file, it will be imported into the current idiom in the importing file.
+\    If you `INCLUDE` this file, it will NOT be imported.
+\    In both cases, the name of the isolated idiom will only be available in its parent.
+\    Therefore, it can only be imported by "related" idioms.
+\  Encapsulate:
+\    The idioms imported by an idiom, and its parents, are NOT imported along with it
+\    when impored into another idiom.  Only its public words.
+\  Export:
+\    The private or public wordlists of the current idiom can be given names that can
+\    be automatically defined within the parent idiom or the Forth wordlist.
+\    Use `@publics | @privates EXPORT-WORDLIST <name>  \ naming convention:  <idiomname>ing`
+\    If for whatever reason both wordlists are desired, export the @privates and import or
+\    extend the idiom.
+
+
 variable 'idiom
 variable breadth  8 breadth !
 variable importing
+variable declared
 
-\ wordlist
-\ create forth-idiom
-\   0 , forth-wordlist , ( wordlist ) , 0 ,
 
 : /idiom  5 cells breadth @ cells + ;
 : @parent  'idiom @ @ ;
@@ -103,16 +148,17 @@ variable importing
 
 : (idiom)
   here  /idiom /allot  8 breadth !
-  ( idiom )  dup extend-idiom  'idiom !
+  ( idiom )  dup extend-idiom  dup 'idiom !  declared !
   wordlist 'idiom @ cell+ !
   wordlist 'idiom @ cell+ cell+ !
   'idiom @ set-idiom  _public ;
 
 : idiom
-  >in @  defined  if   nip  >body  importing @ if  'idiom ! \\ exit
-                                               else  set-idiom  _public  exit  then
+  >in @  defined  if   nip  >body  importing @ if  declared ! \\ exit              \ already defined, importing     => cancel compilation
+                                               else  set-idiom  _public  exit  then \ already defined, not importing => enter / don't create
                   else  drop  >in !  then
-  create  (idiom)  does>  set-idiom  _public ;
+  create  (idiom)  does>  set-idiom  _public ;                                      \ not defined, create
+
 
 : strip-order  get-idiom >r unset-idiom forth-wordlist -order get-order forth-wordlist +order r> set-idiom ;
 
@@ -121,10 +167,10 @@ variable importing
 
 : import
   'idiom @ 0= abort" Can't IMPORT while not inside an idiom!"
-  strip-order  get-current >r  get-idiom >r  importing @ >r  importing on  ['] include catch  r> importing !  throw  'idiom @ r@ add-idiom  r> set-idiom  r> set-current  +orders  ;
+  declared @ >r  strip-order  get-current >r  get-idiom >r  importing @ >r  importing on  ['] include catch  r> importing !  throw  declared @ r@ add-idiom  r> set-idiom  r> set-current  +orders  r> declared ! ;
 : include
   'idiom @ 0= if  include  exit then
-  strip-order  get-current >r  get-idiom >r  include  r> set-idiom r> set-current  +orders ;
+  declared @ >r  strip-order  get-current >r  get-idiom >r  include  r> set-idiom r> set-current  +orders  r> declared ! ;
 
 
 \ create an exposed wordlist out of @publics or @privates in the parent's public wordlist.
